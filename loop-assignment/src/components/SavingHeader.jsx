0e2c6b723c284843b3dd461b44f37a7244a9e6ff
utkarsh-choudhary <utkarshchoudhary734@gmail.com>
2024-07-25 12:33:37 +0530

    initial commits

--- loop-assignment/src/components/SavingHeader.jsx
+++ loop-assignment/src/components/SavingHeader.jsx
@@ -4,9 +4,9 @@ function SavingHeader() {
   return (
     <div className='flex justify-around items-center'>
       <h1 className='text-3xl flex items-start justify-start'>Saving plan details</h1>
-      <div className='p-2 text-xl gap-2  text-purple-500 '>
-      <button className='border p-2 m-4 rounded-lg hover:bg-purple-200'>Scheduled orders</button>
-      <button className='border p-2 rounded-lg hover:bg-purple-200'>Order history</button>
+      <div className='p-2 text-xl gap-2  text-purple-700 '>
+      <button className='border p-4 m-4 rounded-lg hover:bg-purple-200'>Scheduled orders</button>
+      <button className='border p-4 rounded-lg hover:bg-purple-200'>Order history</button>
       </div>
     </div>
   )
